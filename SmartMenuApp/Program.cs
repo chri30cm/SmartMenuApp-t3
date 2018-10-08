ce91dbc9100dbed4d603ae15d5fa3e79cb66c171
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-10-08 23:20:35 +0200

    Add project files.

---
+++ SmartMenuApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+
+namespace SmartMenuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+
+        private void Run()
+        {
+            SmartMenu menu = new SmartMenu();
+            Console.WriteLine("For Dansk: Tryk 1");
+            Console.WriteLine("For English: Press 2");
+            string input = Console.ReadLine();
+
+            int.TryParse(input, out int r);
+            if (r == 0)
+            {
+                Console.WriteLine("Farvel");
+            }
+            else
+            {
+                menu.Load(menu.ChooseLanguage(r));
+                menu.Activate();
+            }
+        }
+    }
+}
